6e279ceb15fb7092359d36b68e35c7df4101b855
dyg <duyangang1230@163.com>
2020-06-21 21:55:44 +0800

    服务注册中心 ：consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/dyg/com/dyg/springcloud/PaymentMain8006.java
@@ -0,0 +1,18 @@
+package com.dyg.com.dyg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author duyangang
+ * @date 2020/6/21 - 11:20
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/dyg/com/dyg/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.dyg.com.dyg.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author duyangang
+ * @date 2020/6/21 - 11:21
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/consul")
+    public String paymentConsul(){
+        return "springcloud with consul: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
