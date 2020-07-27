b961f636968c547d3c341a5028d8380686f8acfd
dyg <duyangang1230@163.com>
2020-07-27 18:35:58 +0800

    动态gateway配置，gateway的filter、route、配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/dyg/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,36 @@
+package com.dyg.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局filter
+ * 自定义filter ：如果没有带参数uname则直接拒绝访问
+ */
+@Configuration
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***************Welcome you into myGlobal filter");
+        String name = exchange.getRequest().getQueryParams().getFirst("uname");
+        if(name == null) {
+            log.info("************用户名为null，非法用户 o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/com/dyg/Test.java
@@ -0,0 +1,11 @@
+package com.dyg;
+
+import java.time.ZonedDateTime;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println(zdt);
+    }
+}
