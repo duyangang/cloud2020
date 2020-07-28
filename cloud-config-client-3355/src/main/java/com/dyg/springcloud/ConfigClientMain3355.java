a178246e6c255b5bd783a2a90e29509551171d7b
dyg <duyangang1230@163.com>
2020-07-29 07:02:27 +0800

    springCloud config

---
+++ cloud-config-client-3355/src/main/java/com/dyg/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,15 @@
+package com.dyg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author duyangang
+ * @date 2020/7/29 - 6:30
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
