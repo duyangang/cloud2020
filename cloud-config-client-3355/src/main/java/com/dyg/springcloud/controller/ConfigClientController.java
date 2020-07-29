33da24fb88b518c8162a65812c9bcf28556b71c9
dyg <duyangang1230@163.com>
2020-07-29 18:41:56 +0800

    配置中心优化

--- cloud-config-client-3355/src/main/java/com/dyg/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/dyg/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,14 @@
 package com.dyg.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+//@RefreshScope + POST刷新（curl -X POST http://localhost:3355/actuator/refresh）
+//如果想自动的让所有client端刷新，config做不到，得需要消息总线支持（Bus）
 public class ConfigClientController {
 
     @Value("${config.info}")
