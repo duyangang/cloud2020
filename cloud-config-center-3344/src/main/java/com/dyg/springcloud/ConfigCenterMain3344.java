95c11cf4dce8ded60daf0a490e61c5df808b19a2
dyg <duyangang1230@163.com>
2020-07-30 14:03:42 +0800

    配置中心优化

--- cloud-config-center-3344/src/main/java/com/dyg/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/dyg/springcloud/ConfigCenterMain3344.java
@@ -8,6 +8,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+/**
+ * 1.半自动的实现刷新配置，缺点是没个config client都得手动刷新，让所有client端刷新，config做不到，得需要消息总线支持（Bus）
+ *
+ * 2.全自动的，消息总线需要刷新：curl -X POST "http://localhost:3344/actuator/bus-refresh"
+ *
+ * 3.只通知某个config-client：curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355"
+ */
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344 .class,args);
