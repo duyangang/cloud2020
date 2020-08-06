ce6e55fe77859db0db81f8836db2110c6085be5f
dyg <duyangang1230@163.com>
2020-08-06 18:17:54 +0800

    alibaba sentinel

--- cloudalibaba-sentinel-service8401/src/main/java/com/dyg/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/dyg/springcloud/controller/FlowLimitController.java
@@ -1,9 +1,11 @@
 package com.dyg.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FlowLimitController
 {
     @GetMapping("/testA")
@@ -13,10 +15,28 @@ public class FlowLimitController
 
     @GetMapping("/testB")
     public String testB() {
-
+        log.info("---execute ..");
         return "------testB";
     }
 
+    @GetMapping("/testC")
+    public String testC() {
+//        log.info("---服务降级规则测试 - RT ..");
+//        try {
+//            Thread.sleep(200);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
+        log.info("---服务降级规则测试 - 异常比例  ..");
+        int a = 10 / 0;
+        return "------testC";
+    }
 
+    @GetMapping("/testD")
+    public String testD() {
+        log.info("---服务降级规则测试 - 异常数  ..");
+        int a = 10 / 0;
+        return "------testC";
+    }
 }
