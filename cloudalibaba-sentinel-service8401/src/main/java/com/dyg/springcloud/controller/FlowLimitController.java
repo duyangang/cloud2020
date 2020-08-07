4a2444cb4deb5d67708a4fb47b779215b4f108c2
dyg <duyangang1230@163.com>
2020-08-07 11:30:12 +0800

    alibaba sentinel

--- cloudalibaba-sentinel-service8401/src/main/java/com/dyg/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/dyg/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.dyg.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +41,17 @@ public class FlowLimitController
         log.info("---服务降级规则测试 - 异常数  ..");
         int a = 10 / 0;
         return "------testC";
+    }
+    @GetMapping("/testE")
+    @SentinelResource(value = "testE",blockHandler="handler_testHostKey")
+    public String testHostKey(@RequestParam(required = false,value = "p1") String p1,
+                            @RequestParam(required = false,value = "p2") String p2) {
+
+        return "testHostKey-----";
+    }
+
+    public String handler_testHostKey(String p1, String p2, BlockException exc) {
+        return "deal testHostKey ";
+
     }
 }
